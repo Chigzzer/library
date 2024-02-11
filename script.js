23e4f66bed14d256353585375f01b54f5362e154
Chiraag Chandarana <42282981+Chigzzer@users.noreply.github.com>
2024-02-11 22:31:26 +0000

    tidied up code

--- script.js
+++ script.js
@@ -85,67 +85,63 @@ function removeBookFromLibrary(){
 
 // Displaying each book with their title only
 function displayBooks(){
-    bookDetails.classList.remove('show');
-    bookDetails.classList.add('hide');
+    bookDetailsToggle();
     librarySection.innerHTML = "";
     myLibrary.forEach((libraryBook, index) =>{
-        let libBook = librarySection.appendChild(document.createElement('div'));
-        libBook.classList.add('book');
-        libBook.dataset.bookIndex = index;
-        let bookTitle = libBook.appendChild(document.createElement('div'));
-        bookTitle.innerText = libraryBook.title;
-        libBook.addEventListener("click", function(){       
-        displayBookDetails(libraryBook, index)
+        createBookDetail(libraryBook, index);       
     });
-})}
+}
 
+
+function createBookDetail(libraryBook, index){
+    let libBook = librarySection.appendChild(document.createElement('div'));
+    let bookTitle = libBook.appendChild(document.createElement('div'));
+    libBook.classList.add('book');
+    libBook.dataset.bookIndex = index;
+    bookTitle.innerText = libraryBook.title;
+    libBook.addEventListener("click", function(){       
+    displayBookDetails(libraryBook, index);
+})
+}
 // Function to open the pop up with the book details.
 function displayBookDetails(libraryBook, index){
-    console.log("Index of book: " + index);
+    // console.log("Index of book: " + index);
     indexDelete = index;
     let bookAuthor = libraryBook.author;
     let bookTitle = libraryBook.title;
     let bookPages = libraryBook.pages;
-    let bookRead = libraryBook.read;
-
     let bookTitleValue = document.querySelector('#book-title-value');
     let bookAuthorValue = document.querySelector('#book-author-value');
     let bookPagesValue = document.querySelector('#book-pages-value');
     let bookReadButton = document.querySelector('#book-read');
-    let removeButton = document.querySelector('#delete-book');
-
-
+   
     displayBookRead(libraryBook, bookReadButton);
     bookReadButton.addEventListener("click", function(){
-        if (libraryBook.read == true){
-            libraryBook.read = false;
-        }
-        else{
-            libraryBook.read=true;
-        }
+        toggleReadState(libraryBook);
         displayBookRead(libraryBook, bookReadButton);
     })
-
-
 
     bookTitleValue.innerText = bookTitle;
     bookAuthorValue.innerText = bookAuthor;
     bookPagesValue.innerText = bookPages;
 
-
-    bookDetails.classList.remove('hide');
-    bookDetails.classList.add('show');
+    bookDetailsToggle();
 }
 
-function deleteTest(){
-    console.log(indexDelete);
+function bookDetailsToggle(){
+    bookDetails.classList.toggle('show');
 }
 
+function toggleReadState(book){
+    if (book.read == true){
+        book.read = false;
+    }
+    else{
+        book.read = true;
+    }
+}
 
 function displayBookRead(book, bookChangeButton){
-    console.log("TEST");
-    console.log(book.read);
-    bookChangeButton.classList.toggle('book-read');
     if (book.read == true){
         bookChangeButton.innerText = "Read";
         bookChangeButton.classList.add('book-read');
@@ -160,7 +156,7 @@ function displayBookRead(book, bookChangeButton){
 
 // Test section to add books to library:
 let book1 = new Book('Uno', 'Chiraag', 456, true);
-let book2 = new Book('Duo', 'Chiraag', 345, true);
+let book2 = new Book('Duo', 'Chiraag', 345, false);
 let book3 = new Book('Thres', 'Chiraag', 200, false);
 let book4 = new Book('Thres', 'Chiraag', 200, false);
 let book5 = new Book('Thres', 'Chiraag', 200, false);
